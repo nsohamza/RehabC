1852bc7ac09ca386738217f42a5ed5850080cccd
nsohamza <smh16@protonmail.com>
2025-05-15 09:57:27 +0200

    navbar,herosection,index

---
+++ rehabcenter/src/Components/HeroSection.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import React from 'react'
+import familyImage from '@/assets/images.jpeg'
+
+const HeroSection = () => {
+  return (
+    <section className=' bg-[#cbe2ee] flex justify-between justify-items-center´'>
+     <div className=' w-1/2 ml-8 mt-12  '>
+        <p className='mb-4 text-xl uppercase tracking-wider' >we´re here to help you</p>
+        <h1 className='text-8xl font-semibold leading-tight capitalize ml-10'>pick yourself <br/>back up - <br/> you´ve got this</h1>
+     </div>
+
+     <div className=' w-3/4 relative h-[900px] top-15 '>
+       <Image 
+       src={familyImage}
+       alt="Image of family" 
+       fill
+       className='object-cover'
+       />
+
+       <div className=' absolute bottom-10 left-10 items-start '>
+       <h2 className='text-4xl italic text-black text-center tracking-wider '>Start a new chapter of your life, today.</h2>
+       <button className='uppercase bg-[#bde1f7] hover:bg-blue-700 text-xl rounded-4xl py-3 px-5 mt-3 font-semibold'>call 24/7: 123-456-7890</button>
+       </div>
+
+     </div>
+
+    </section>
+  )
+}
+
+export default HeroSection
